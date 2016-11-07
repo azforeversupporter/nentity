3c341a52019072c709cb521f38e892bf42505cbe
azforeversupporter <azforeversupporter@gmail.com>
2016-11-07 22:38:29 +0100

    Added exceptions

---
+++ src/exceptions/index.ts
@@ -0,0 +1,34 @@
+export class ArgumentNullException extends Error {
+
+    public constructor(message?: string|undefined) {
+        super(message);
+    }
+}
+
+export class InvalidOperationException extends Error {
+
+    public constructor(message?: string|undefined) {
+        super(message);
+    }
+}
+
+export class InvalidArgumentException extends Error {
+
+    public constructor(message?: string|undefined) {
+        super(message);
+    }
+}
+
+export class NullReferenceException extends Error {
+
+    public constructor(message?: string|undefined) {
+        super(message);
+    }
+}
+
+export class Exception extends Error {
+
+    public constructor(message?: string|undefined) {
+        super(message);
+    }
+}
